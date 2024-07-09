e29fc2c21f4527dccb9ee3010cffea11ebf95a87
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-07-09 20:44:11 -0300

    test ci/cd

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 
 const HomePage = () => {
-  return <Typography variant="h1">Home page</Typography>;
+  return <Typography variant="h1">Home page!</Typography>;
 };
 
 export default HomePage;
